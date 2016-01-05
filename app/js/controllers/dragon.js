3f14cadac075c8e7e44c33db5ddbe4cc6a4c7075
Grzegorz Daszuta <grzegorz.daszuta@codewave.pl>
2016-01-05 09:50:40 +0100

    further deoptimisation

--- app/js/controllers/dragon.js
+++ app/js/controllers/dragon.js
@@ -3,6 +3,14 @@
 function DragonCtrl($scope, $interval, $log) {
   'ngInject';
 
+  function expensiveSleep(ms) {
+    var start = new Date().getTime(), expire = start + ms;
+    while (new Date().getTime() < expire) { 
+        var j = Math.sqrt(Math.random());
+    }
+    return;
+  }
+
   const vm = this;
   vm.title = 'Dragon';
 
@@ -15,8 +23,9 @@ function DragonCtrl($scope, $interval, $log) {
   $interval(() => {
     if(Math.random() < 0.05) {
         $log.debug('Dragon poof!', angular.element('#DRAGON div').last().remove());
+        expensiveSleep(200);
     }
-  }, 10);
+  }, 1);
 }
 
 export default {
